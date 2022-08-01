6d9f082e2924fb0a26cf11430bc9add78314bd53
Paul Jung <pbnjcub@gmail.com>
2022-08-01 16:18:30 -0400

    lab done

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+//   iterate from the beginning to the middle of the word.
+  for (let i = 0; i < word.length / 2; i++) {
+    //check each letter to the corresponding letter from the end
+    const j = word.length - 1 - i
+    const startChar = word[i]
+    const endChar = word[j]
+      //if any letters don't match, return false
+    if (startChar !== endChar) return false
+  }
+  return true
+}
+
+/*
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+// return true
+
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning to the middle of the word.
+    check each letter to the corresponding letter from the end
+      if any letters don't match, return false
+  
+  return true
 */
 
 /*
@@ -13,13 +36,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("willow"));
 
   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("amma"));
 }
 
 module.exports = isPalindrome;
